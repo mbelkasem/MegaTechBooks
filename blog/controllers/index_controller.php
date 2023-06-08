493e5d25ec02b39dbc7f93427fb01592b77b8c13
Zakariya <z.jadimoussa@hotmail.fr>
2023-06-08 06:36:57 +0200

    index methode MVC

--- blog/controllers/index_controller.php
+++ blog/controllers/index_controller.php
@@ -1,10 +1,10 @@
 <?php
 // Inclure le modèle
-$data = include 'admin_model.php';
+$data = include 'index_model.php';
 
 // Extraire les données du modèle
 extract($data);
 
 // Inclure la vue
-include 'admin_view.php';
+include 'index_view.php';
 ?>
