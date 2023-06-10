e315d2537a9a4f4034468c8e81796d44012bbc36
Samir Boudekhan <samirboudekhan@hotmail.com>
2023-06-10 13:56:41 +0200

    Crud Product

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use PhpParser\Node\Name;
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Text;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            AssociationField::new('categories', 'Category'),
+            TextField::new('name'),
+            TextField::new('description'),
+            NumberField::new('price')
+            ->setNumDecimals(2),
+            IntegerField::new('stock'),
+            DateTimeField::new('created_at', 'Schedule')
+            ->setFormTypeOptions([
+                'widget' => 'single_text',
+                'html5' => true,
+                'attr' => ['class' => 'js-flatpickr'],
+            ])
+            ->setCustomOption('widget', 'single_text')
+            ->setCustomOption('format', 'yyyy-MM-dd HH:mm:ss')
+            ->setCustomOption('locale', 'fr')
+            ->setRequired(true),
+            
+            // ImageField::new('image_url')->setBasePath('/images/categories'),
+
+            ImageField::new('image_url')->setUploadDir('public/images/categories/')
+            ->setUploadedFileNamePattern('[Name]'),
+
+            // TextEditorField::new('description'),
+
+        
+        ];
+    }
+    
+}
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -73,4 +73,10 @@ class Category
 
         return $this;
     }
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
+
+
 }
