216fe66b572f59972e173c7e9ed3ca0b501cd8bc
MBELKASEM <mohamed.belkasem@gmail.com>
2023-06-10 16:57:20 +0200

    Contact Form complete

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(HttpFoundationRequest $request, MailerInterface $mailer): Response
     {
         $contactForm = $this->createForm(ContactType::class);
 
+        $contactForm->handleRequest($request);
+        if ($contactForm->isSubmitted() && $contactForm->isValid()) {
+           $address = $contactForm->get('email')->getData();
+           $subject = $contactForm->get('subject')->getData();
+           $content = $contactForm->get('content')->getData();
+
+           $email = (new Email())
+            ->from($address)
+            ->to('contact@megatechbooks.be')          
+            ->subject($subject)
+            ->text($content);
+            
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Email envoyé ');
+            return $this->redirect('/'); 
+        }
        
        
         return $this->render('contact/index.html.twig', [
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,18 +5,25 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType; // Import the correct namespace
+use Symfony\Component\Form\Extension\Core\Type\TextType; // Import the correct namespace
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Twig\Node\Expression\Binary\SubBinary;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('subject')
-            ->add('content', TextareaType::class)
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail'
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Sujet'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Message'
+            ])
             ->add('confirm', SubmitType::class)
         ;
     }
