6017040f1fe2dcd4915a9218788334a774bc2d84
MBELKASEM <mohamed.belkasem@gmail.com>
2023-06-11 12:08:55 +0200

    Boostrap template

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -25,12 +25,13 @@ class CategoryController extends AbstractController
     
 
     #[Route('/{id}', name: 'app_category_show', methods: ['GET'])]
-    public function show(Category $category): Response
+    public function show(Category $category, CategoryRepository $categoryRepository): Response
     {
         $article=$category->getPosts();
         return $this->render('category/show.html.twig', [
             'category' => $category,
             'articles' => $article,
+            'categories' => $categoryRepository->findAll(),
         ]);
     }    
 
