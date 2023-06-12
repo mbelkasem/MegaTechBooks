71b0b1ad1f39e3a9dd61c8c8c5ba00c4b4f2e0f2
MBELKASEM <mohamed.belkasem@gmail.com>
2023-06-12 19:09:51 +0200

    Update Product Template

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -37,6 +37,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Produits', 'fas fa-list', Product::class);
+        //yield MenuItem::linkToCrud('Blog', 'fas fa-list', Post::class);
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-list', User::class);
         // yield MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class);
 
