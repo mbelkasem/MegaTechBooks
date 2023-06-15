18081e88cf8086b13ccf0af1790b39ec82831e2a
mohafor <mkechibil@gmail.com>
2023-06-15 19:56:02 +0200

    templetes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -18,4 +18,28 @@ class ProductController extends AbstractController
             'products' => $products,
         ]);
     }
+    #[Route('/', name: 'app_books')]
+    public function books(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+        return $this->render('product/books.html.twig', [
+            'products' => $products,
+        ]);
+    }  
+    #[Route('/', name: 'app_computers')]
+    public function computers(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+        return $this->render('product/computers.html.twig', [
+            'products' => $products,
+        ]);
+    }    
+    #[Route('/', name: 'app_hiffis')]
+    public function hiffis(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+        return $this->render('product/hiffis.html.twig', [
+            'products' => $products,
+        ]);
+    }      
 }
